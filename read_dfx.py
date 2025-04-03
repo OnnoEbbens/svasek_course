74e475e041323b9f5177ba0a091083328c19d71b
yannicksteenman <77574349+yannicksteenman@users.noreply.github.com>
2025-04-03 13:30:12 +0200

    Generalizing reading
    More generalized reading of points

--- read_dfx.py
+++ read_dfx.py
@@ -13,7 +13,7 @@ import ezdxf
 from collections import Counter
 
 # %% Input
-filename = 'oostende_2d.dxf'
+filename = '../oostende_2d.dxf'
 
 # %% Functions
 # Read DXF file and get info
@@ -21,56 +21,52 @@ doc = ezdxf.readfile(filename)
 msp = doc.modelspace()
 
 # Get the amount of different entities in the file
-# entities = Counter()
-# for entity in msp:
-#     entities[entity.dxftype()] += 1
-#     print(type(entity))
+entities = Counter()
+for entity in msp:
+    entities[entity.dxftype()] += 1
+    print(type(entity))
 
-# print(f"Entities in {filename}:")
-# for entity_type, count in entities.items():
-#     print(f"{entity_type}: {count}")
+print(f"Entities in {filename}:")
+for entity_type, count in entities.items():
+    print(f"{entity_type}: {count}")
 
-# # Get the no. of dimensions
-# is_3d = False
-# for entity in entities:
-#     for geometry in msp.query(entity):
-#         try:
-#             start = geometry.dxf.start
-#             if start[2] >= 1E-6:
-#                 is_3d = True
-#                 print(start)
-#                 break  
-#         except ezdxf.DXFAttributeError:
-#             continue
+# Get the no. of dimensions
+is_3d = False
+for entity in entities:
+    for geometry in msp.query(entity):
+        try:
+            start = geometry.dxf.start
+            if start[2] >= 1E-6:
+                is_3d = True
+                print(start)
+                break  
+        except ezdxf.DXFAttributeError:
+            continue
 
-# if is_3d:
-#     print("3D DXF file detected.")
-# else:
-#     print("2D DXF file detected.")
+if is_3d:
+    print("3D DXF file detected.")
+else:
+    print("2D DXF file detected.")
 
 
+# %% Split the entities
 
-# def split_dxf_into_lines(msp: ezdxf.layouts.layout.Modelspace):
-polylines = []
+lines = []
+for entity in msp:
+    try:
+        if entity.dxftype() == 'LINE':
+            linepoints = [entity.dxf.start, entity.dxf.end]
+            lines.append(linepoints)
+        elif entity.dxftype() == 'LWPOLYLINE':
+            points = entity.get_points('xyz')
+            linepoints = [point for point in points]
+            lines.append(linepoints)
+        elif entity.dxftype() == 'POLYLINE':
+            print(entity.points())
+            # points_list = [point[:2] for point in entity.points()]
 
-for polyline in msp.query('POLYLINE'):
-    i_polyline = []
-    # Get the vertices of the polyline
-    vertices = polyline.vertices
-    # Split the polyline into individual line segments
-    for i in range(len(vertices) - 1):
-        start_point = vertices[i].format()
-        end_point = vertices[i + 1].format()
-        i_polyline.append([start_point, end_point])
-    polylines.append(i_polyline)
-
-lw_polylines = []
-for lwpolyline in msp.query('LWPOLYLINE'):
-    i_lwpolyline = []
-    # Get the vertices of the polyline
-    for vertex in lwpolyline.vertices_in_wcs():
-        i_lwpolyline.append(vertex.format())
-    lw_polylines.append(i_lwpolyline)
+    except Exception as e:
+        print(f"Error processing entity {entity}: {e}")
 
 
 # %%
